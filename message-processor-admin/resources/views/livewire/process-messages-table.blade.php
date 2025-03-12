a6a99609e2014ac5ee3c6133266f775e24d1b7a6
Khorram Khondkar <khorramk.kbsk@gmail.com>
2025-03-12 11:47:44 +0000

    Message process admin UI table: created

--- message-processor-admin/resources/views/livewire/process-messages-table.blade.php
+++ message-processor-admin/resources/views/livewire/process-messages-table.blade.php
@@ -1,35 +1,22 @@
-<div class="overflow-x-auto">
-    <table class="table">
+<div class="overflow-x-auto text-center">
+    <h1 class="text-4xl font-bold mb-7 mt-2.5 text-gray-400">
+        Process the message please
+    </h1>
+    <table class="table text-gray-400">
       <!-- head -->
-      <thead>
+      <thead class="text-gray-400">
         <tr>
-          <th></th>
-          <th>Name</th>
-          <th>Job</th>
-          <th>Favorite Color</th>
+          <th class="border border-gray-400">ID</th>
+          <th class="border border-gray-400">Message Title</th>
+          <th class="border border-gray-400">Action</th>
         </tr>
       </thead>
       <tbody>
         <!-- row 1 -->
         <tr>
-          <th>1</th>
-          <td>Cy Ganderton</td>
-          <td>Quality Control Specialist</td>
-          <td>Blue</td>
-        </tr>
-        <!-- row 2 -->
-        <tr>
-          <th>2</th>
-          <td>Hart Hagerty</td>
-          <td>Desktop Support Technician</td>
-          <td>Purple</td>
-        </tr>
-        <!-- row 3 -->
-        <tr>
-          <th>3</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
+          <th class="border border-gray-400">1</th>
+          <td class="border border-gray-400">Hello there</td>
+          <td class="border border-gray-400"><button class="btn">Go</button></td>
         </tr>
       </tbody>
     </table>
