60b40238ca656046fdfb8257f5d28b9d35970d10
Khorram Khondkar <khorramk.kbsk@gmail.com>
2025-03-12 11:05:44 +0000

    Livewire process message table

---
+++ message-processor-admin/app/Livewire/ProcessMessagesTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ProcessMessagesTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.process-messages-table');
+    }
+}
