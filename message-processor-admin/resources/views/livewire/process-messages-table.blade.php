ff08466d3e0a7f51153ea7261e5331106cf95c18
Khorram Khondkar <khorramk.kbsk@gmail.com>
2025-03-13 16:16:48 +0000

    Showing paginated messages data

--- message-processor-admin/resources/views/livewire/process-messages-table.blade.php
+++ message-processor-admin/resources/views/livewire/process-messages-table.blade.php
@@ -3,7 +3,6 @@
         Process the message please
     </h1>
     <table class="table text-gray-400">
-      <!-- head -->
       <thead class="text-gray-400">
         <tr>
           <th class="border border-gray-400">ID</th>
@@ -12,12 +11,17 @@
         </tr>
       </thead>
       <tbody>
-        <!-- row 1 -->
-        <tr>
-          <th class="border border-gray-400">1</th>
-          <td class="border border-gray-400">Hello there</td>
-          <td class="border border-gray-400"><button class="btn">Go</button></td>
-        </tr>
+        @foreach ($messages as $message)
+          <tr>
+            <th class="border border-gray-400">{{ $message->id }}</th>
+            <td class="border border-gray-400">{{ $message->message }}</td>
+            <td class="border border-gray-400"><button class="btn">Go</button></td>
+          </tr>
+        @endforeach
+
       </tbody>
     </table>
+
+    {{ $messages->links() }}
+
   </div>
