8d01337ef7e479806e4dcc7227312d5a95ea8e6d
Khorram Khondkar <khorramk.kbsk@gmail.com>
2025-03-14 11:35:41 +0000

    Go function to process messages

--- message-processor-admin/app/Livewire/ProcessMessagesTable.php
+++ message-processor-admin/app/Livewire/ProcessMessagesTable.php
@@ -4,9 +4,11 @@ namespace App\Livewire;
 
 use App\Models\Messages;
 use Livewire\Component;
-
+use Livewire\WithPagination;
+use Livewire\Attributes\On;
 class ProcessMessagesTable extends Component
 {
+    use WithPagination;
 
     public function render()
     {
@@ -14,5 +16,24 @@ class ProcessMessagesTable extends Component
         return view('livewire.process-messages-table', [
             'messages' => $messages
         ]);
+    }
+
+    public function go(int $id)
+    {
+        try {
+            $message = Messages::query()->find($id);
+            $message->processed = true;
+            $message->save();
+
+            //TODO broadcast to the message user if you can
+
+            session()->flash('processed', true);
+        } catch (\Exception $exception) {
+            report($exception);
+
+            session()->flash('failed', true);
+
+        }
+
     }
 }
