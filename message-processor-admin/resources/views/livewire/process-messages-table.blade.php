148178a06a2f243aa02ae36ddb7c4655a2f04174
Khorram Khondkar <khorramk.kbsk@gmail.com>
2025-03-14 17:20:14 +0000

    Showing the message every 6 second

--- message-processor-admin/resources/views/livewire/process-messages-table.blade.php
+++ message-processor-admin/resources/views/livewire/process-messages-table.blade.php
@@ -1,29 +1,36 @@
 {{-- polling every 30s to make sure there is a slight delay for oncoming messages --}}
-<div class="overflow-x-auto text-center" wire:poll.30s>
+<div class="overflow-x-auto text-center" wire:poll.6s>
     <h1 class="text-4xl font-bold mb-7 mt-2.5 text-gray-400">
         Process the message please
     </h1>
+    @if ($messages->count() > 0 )
     <table class="table text-gray-400">
-      <thead class="text-gray-400">
-        <tr>
-          <th class="border border-gray-400">ID</th>
-          <th class="border border-gray-400">Message Title</th>
-          <th class="border border-gray-400">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-
-        @foreach ($messages as $message)
+        <thead class="text-gray-400">
           <tr>
-            <th class="border border-gray-400">{{ $message->id}}</th>
-            <td class="border border-gray-400">{{ $message->message }}</td>
-            <td class="border border-gray-400"><button class="btn" wire:click="go($message->id)">Go</button></td>
+            <th class="border border-gray-400">ID</th>
+            <th class="border border-gray-400">Message Title</th>
+            <th class="border border-gray-400">Action</th>
           </tr>
-        @endforeach
+        </thead>
+        <tbody>
 
-      </tbody>
-    </table>
+          @foreach ($messages as $message)
+            <tr>
+              <th class="border border-gray-400">{{ $message->id}}</th>
+              <td class="border border-gray-400">{{ $message->message }}</td>
+              <td class="border border-gray-400"><button class="btn" wire:click="go({{$message->id}})">Go</button></td>
+            </tr>
+          @endforeach
+
+        </tbody>
+      </table>
 
     {{ $messages->links() }}
+
+    @else
+      <div class="div">
+        <h2>No messages for processing at the moment!</h2>
+      </div>
+      @endif
 
   </div>
